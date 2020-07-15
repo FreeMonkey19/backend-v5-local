53da0b8a2ddd1926995c683b3e5944eb713d555a
freeMonkey19 <freeMonkey19@gmail.com>
2020-07-14 22:16:36 -0700

    auto generated files and auth.py cleanup

--- api/auth.py
+++ api/auth.py
@@ -3,22 +3,10 @@ from . import db
 
 auth= Blueprint('auth', __name__)
 
-# add user to db
-# @auth.route("/registrations", methods=['POST'])
-# def registration():
-#     user_data = request.get_json()
-#     new_user = site_users(name=user_data['name'])
 
-#     db.session.add(new_user)
-#     db.session.commit()
-
-#     return 'User Successfully Added', 201
-#     return 'Registrations'
-
-
-@auth.route('/login')
+@auth.route('/logged_in')
 def login():
-    return 'Login'
+    return 'Logged In!'
 
 
 @auth.route('/signup')
